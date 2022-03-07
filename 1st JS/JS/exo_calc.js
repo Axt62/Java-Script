6927c028e5dfd44a1d6f4bd786cddea3822878aa
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-07 17:10:50 +0100

    Exo opérateurs

--- 1st JS/JS/exo_calc.js
+++ 1st JS/JS/exo_calc.js
@@ -1,3 +1,22 @@
 function calcul(formulaire){
     formulaire.res.value = eval(formulaire.op.value)
+};
+
+function calculer(form){
+    let R1 = parseFloat (form.R1.value);
+    const R2 = parseFloat (form.R2.value);
+
+    let c = R1;
+    form.res.value = toStr("+", R1 += R2);
+    R1 = c;
+    form.res.value += toStr("*", R1 *= R2);
+    R1 = c;
+    form.res.value += toStr("/", R1 /= R2);
+    R1 = c;
+    form.res.value += toStr("%", R1 %= R2);
+
 }
+
+function toStr(op, res){
+    return `Àprès l'opération R1 ${op}= R2, R1 a pour valeur ${res}\n`;
+} 

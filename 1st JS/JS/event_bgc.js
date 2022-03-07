657328956f6810a776a1d2240caec0712fdd77ab
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-07 11:45:18 +0100

    Input JS

---
+++ 1st JS/JS/event_bgc.js
@@ -0,0 +1,21 @@
+function changeBgc(color){
+    document.body.style.backgroundColor = 'color';
+
+}
+
+function f(form){
+    form.prenom.style.backgroundColor = 'red';
+}
+
+function f1(form){
+    form.prenom.style.backgroundColor = 'white';
+}
+
+function text(form){
+    console.log(form.prenom.value);
+    form.out.innerText = val;
+}
+
+function mouseHover(){
+    document.formulaire_test.prenom.style.backgroundColor = 'yellow';
+}

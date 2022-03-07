2318b811afcaba9b1d80fe50d8c6adfbc4b1b11b
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-07 10:15:13 +0100

    Function/Consol log/Document.write

---
+++ 1st JS/JS/index.js
@@ -0,0 +1,16 @@
+/* document.write ("Ce texte est écrit par une instruction JavaScript");
+
+alert('Je suis une alerte') 
+
+console.log('Je suis un console log'); */
+
+/* function carre(nombre) {
+    console.log (nombre * nombre);
+  }; 
+carre(19); */
+
+function carre(nombre) {
+    return nombre * nombre;
+}
+
+console.log(carre(16));

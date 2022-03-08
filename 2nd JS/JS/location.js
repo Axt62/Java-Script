0e14ff9f872cfbd51cab19c0e38d4e3118c10db9
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-08 17:10:10 +0100

    Location/History

--- 2nd JS/JS/location.js
+++ 2nd JS/JS/location.js
@@ -1,1 +1,15 @@
-console.log(/*window.*/location)
+console.log(location);
+//location.href = 'https://google.fr';
+
+const liste = document.getElementById('liste');
+//debugger;
+
+
+liste.innerHTML = `
+    <li>Protocol : ${location.protocol}</li>
+    <li>Hostname : ${location.hostname}</li>
+    <li>Port : ${location.port}</li>
+    <li>Pathname : ${location.pathname}</li>`;
+
+    console.log(history);
+

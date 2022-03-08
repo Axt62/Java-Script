f80f13c42f3fc0716ccc3f67025aa8704032d0e8
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-08 12:04:47 +0100

    Iterations

---
+++ 2nd JS/JS/iterations.js
@@ -0,0 +1,21 @@
+/*for (let i = 0; i < 20; i++){
+    if (i % 3 === 0){
+        continue;
+    }
+    if (i === 17){
+        break;
+    }
+    document.write(`<h5>${i}<h5>`)
+    
+}*/
+
+const tab1 = ["sam", "herve", "sab", "anto"];
+
+/*for(let i = 0; i < tab1.length; i++){
+    document.writeln(tab1[i]);
+};*/
+
+
+/*for (const prenom of tab1){
+    document.writeln(prenom);
+}*/

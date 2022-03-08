9e2a77573713108bf2dec7ce8f81993a2950d2fd
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-08 19:01:06 +0100

    Ajout de commentaires sur la fiche JS location

--- 2nd JS/JS/location.js
+++ 2nd JS/JS/location.js
@@ -1,10 +1,12 @@
 console.log(location);
 //location.href = 'https://google.fr';
 
+
+// Formation de la liste en lien avec le html
 const liste = document.getElementById('liste');
 //debugger;
 
-
+//Fournis les informations ( Protocol : http: , host , port et le chemin)
 liste.innerHTML = `
     <li>Protocol : ${location.protocol}</li>
     <li>Hostname : ${location.hostname}</li>

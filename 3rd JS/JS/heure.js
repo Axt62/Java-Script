163f2926305afc42903bae0826fe4e76c3ab7aea
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-09 15:36:35 +0100

    Heures opti

--- 3rd JS/JS/heure.js
+++ 3rd JS/JS/heure.js
@@ -4,8 +4,13 @@ const secondSpan = document.getElementById('second');
 
 function getTime(){
     const date = new Date();
-    hourSpan.innerText = inf10TwoChar(date.getHours().toString());
-    minuteSpan.innerText = inf10TwoChar(date.getMinutes().toString());
+
+   if (hourSpan.innerText !== inf10TwoChar(date.getHours().toString())){
+       hourSpan.innerText = inf10TwoChar(date.getHours());
+   }
+   if (minuteSpan.innerText !== inf10TwoChar(date.getMinutes().toString())){
+        hourSpan.innerText = inf10TwoChar(date.getHours());
+   }
     secondSpan.innerText = inf10TwoChar(date.getSeconds().toString());
 }
 
@@ -16,4 +21,30 @@ function inf10TwoChar(date){
 getTime();
 
 setInterval(getTime, 1000);
+
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 

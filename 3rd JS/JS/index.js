1c5ebd9ae8022ed54478ec3006ab3479fa59ef7d
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-09 12:04:14 +0100

    ToDoList done

---
+++ 3rd JS/JS/index.js
@@ -0,0 +1,8 @@
+function addToTdl(){
+
+const liste = document.getElementById('liste');
+
+
+liste.innerHTML += `<li>${document.getElementById("Tdl").value}</li>`;
+
+}
---
+++ 3rd JS/JS/navigator.js
@@ -0,0 +1,4 @@
+document.write(navigator.appCodeName + "<br>");
+document.write(navigator.appName + "<br>");
+document.write(navigator.appVersion + "<br>");
+document.write(navigator.userAgent + "<br>");

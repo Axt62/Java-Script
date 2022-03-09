86418f486d4696e338e427a1aa2afcfb7c5972ca
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-09 15:12:55 +0100

    Heures

--- 3rd JS/JS/heure.js
+++ 3rd JS/JS/heure.js
@@ -1,10 +1,19 @@
 const hourSpan = document.getElementById('hour');
 const minuteSpan = document.getElementById('minute');
-const secondSpan = document.getElementById('seond');
+const secondSpan = document.getElementById('second');
 
 function getTime(){
     const date = new Date();
-    hourSpan.innerText = date.getHours().toString();
-    minuteSpan.innerText = date.getMinutes().toString();
-    secondSpan.innerText = date.getSeconds().toString();
+    hourSpan.innerText = inf10TwoChar(date.getHours().toString());
+    minuteSpan.innerText = inf10TwoChar(date.getMinutes().toString());
+    secondSpan.innerText = inf10TwoChar(date.getSeconds().toString());
 }
+
+function inf10TwoChar(date){
+    return date < 10 ? `0${date}` : date.toString();
+}
+
+getTime();
+
+setInterval(getTime, 1000);
+

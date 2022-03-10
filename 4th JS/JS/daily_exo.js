9764584c1b9b6457407d96e53ac989ac9c9ff38d
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-10 10:23:26 +0100

    Exo anniversaire

---
+++ 4th JS/JS/daily_exo.js
@@ -0,0 +1,24 @@
+function print(...str){
+    document.writeln(str + "<br>");
+}
+
+
+const res = document.getElementById("res");
+
+
+function anniversaire(form) {
+    const birthday = new Date(form.birthday.value);
+    const today = new Date();
+
+    const bMonth = birthday.getMonth();
+    const bDay = birthday.getDate();
+    // console.log(bMonth, bDay);
+};
+
+
+
+
+
+
+
+

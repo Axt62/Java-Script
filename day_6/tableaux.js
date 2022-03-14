5d9b5812f102de3f2b7e2ee31cd331d2f85bca84
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-14 12:10:43 +0100

    Tableaux

--- day_6/tableaux.js
+++ day_6/tableaux.js
@@ -1,2 +1,45 @@
 let t = [12, 34, "coucou", false];
 console.log(t)
+let [prenom, nom, age] = ["sam", "mich", 40];
+console.log(nom, prenom, age);
+let t1 = new Array(3);
+
+
+// Ajouter dans un tableau
+
+t.push("ajout");
+console.log(t);
+
+// Suppression du dernier élément
+
+t.pop();
+console.log(t);
+
+// Suppression du dernier élémént
+
+t.shift();
+console.log(t);
+
+// Insertion en début de tableau 
+
+t.unshift("Samuel");
+console.log(t);
+
+// Suppression avec un index
+
+t.splice(2, 1);
+console.log(t);
+
+// Copie de tableau   
+let t2 = [...t];
+let t3 = t.slice();
+console.log(t2, t3);
+
+t.push(12, 56, 98, -34, -27, 0);
+console.log(t);
+
+for (let i = 0; i <t.length; i++) {
+    if (typeof t [i] !== 'number') {
+        t.splice(i, 1)
+    }
+}

78394ac9035ce6f4a0a0fb7894930aadb54561aa
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-15 08:29:00 +0100

    All good

--- day_6/tableaux.js
+++ day_6/tableaux.js
@@ -4,33 +4,27 @@ let [prenom, nom, age] = ["sam", "mich", 40];
 console.log(nom, prenom, age);
 let t1 = new Array(3);
 
-
-// Ajouter dans un tableau
-
+// ajouter dans un tableau
 t.push("ajout");
 console.log(t);
 
-// Suppression du dernier élément
-
+// suppression du dernier élément
 t.pop();
 console.log(t);
 
-// Suppression du dernier élémént
-
+// suppression premier élément
 t.shift();
 console.log(t);
 
-// Insertion en début de tableau 
-
-t.unshift("Samuel");
+// insertion en début de tableau
+t.unshift("samuel");
 console.log(t);
 
-// Suppression avec un index
-
+// suppression avec un index
 t.splice(2, 1);
 console.log(t);
 
-// Copie de tableau   
+// copie de tableau
 let t2 = [...t];
 let t3 = t.slice();
 console.log(t2, t3);
@@ -38,20 +32,26 @@ console.log(t2, t3);
 t.push(12, 56, 98, -34, -27, 0);
 console.log(t);
 
-/*for (let i = 0; i <t.length; i++) {
-    if (typeof t [i] !== 'number') {
-        t.splice(i, 1)
-    }
-}
-console.log(t) */
+// for (let i = 0; i < t.length; i++) {
+//     if (typeof t[i] !== 'number'){
+//         t.splice(i, 1);
+//     }
+// }
+// console.log(t);
 
-// t = t.filter(value =>(typeof value === 'number'));
-// console.log(t)
+// t = t.filter(value => (typeof value === 'number'));
+// console.log(t);
+
+// t.forEach((value, index, arr) => {
+//     if (typeof value !== 'number') {
+//         arr.splice(index, 1);
+//     }
+// });
 
 let t5 = t.map((value) => {
     if (typeof value === 'number') {
-        return value ** 3
+        return value ** 3;
     }
-})
+});
 
-console.log(t5)
+console.log(t5);

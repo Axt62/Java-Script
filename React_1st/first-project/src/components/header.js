2bab776b6e4a3cf233eea12e02b97139a985457a
Antoine Leconte <antoine.leconte9462.pro@gmail.com>
2022-03-21 11:08:04 +0100

    Header & Navbar

---
+++ React_1st/first-project/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavBar} from './navbar'
+
+export const Header = () => {
+
+
+
+    return (
+        <header>
+            <h1>Je suis le Header</h1>
+            <NavBar />
+        </header>
+    );
+}
---
+++ React_1st/first-project/src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export const NavBar = () => {
+
+    return (
+        <div className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Articles</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+
+}
